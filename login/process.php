2e6d2a928b84ccdd45507f89047ad3ac6bb307e3
techybyte <info@techybyte.co.uk>
2016-12-08 18:50:16 +0000

    Made better

--- login/process.php
+++ login/process.php
@@ -15,26 +15,34 @@ if (!isset($_POST["password"])) {
 
 $db = new db();
 $code = strtoupper($_POST["code"]);
+$usrPassword = "";
+$usrId = "1234567890";
 
-$usrRec = $db->queryForRow("SELECT id, password FROM `users` WHERE `code` = '" . $code . "';");
+$db->setDbObj();
+$pps = $db->obj->prepare("SELECT id, password FROM `users` WHERE `code`=?");
+$pps->bind_param('s', $code);
+$pps->execute();
+$pps->bind_result($usrId, $usrPassword);
+$pps->fetch();
 
-if (count($usrRec) == 0) {
+if ($usrId == "1234567890") {
     redirect("login/?n=nx"); //NON-EXISTENT USER notice
     exit();
 }
 
-if (!(password_verify($_POST["password"], $usrRec["password"]))) {
+if (!(password_verify($_POST["password"], $usrPassword))) {
     redirect("login/?n=ic"); //INCORRECT PASSWORD notice
     exit();
 }
 
 $session = new session();
-$session->newSession($usrRec["id"]);
+$session->newSession($usrId);
+
+$pps->close();
 
 if ($_POST["password"]=="password") {
     redirect("home/account/password?n=1t");
 } else {
     redirect("home/");
 }
-
 
