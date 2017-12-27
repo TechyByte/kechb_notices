57e4dba66f4af47f6c29a91daaf386f5083e2031
techybyte <info@techybyte.co.uk>
2017-12-27 17:48:17 +0000

    V1-3

--- functions/mysql.php
+++ functions/mysql.php
@@ -1,15 +1,19 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(E_ALL);
+
 class db {
     public function connect() {
-        $mysqlObj = new mysqli("localhost", "id217864_notices", "<REDACTED>", "id217864_notices");
+        $mysqlObj = new mysqli("localhost", "id217864_notices", "vUDg6m4LVyAnq3JG", "id217864_notices");
+        //$mysqlObj = new mysqli("aa1aakoygzej97l.cubzqbptcrhl.eu-west-1.rds.amazonaws.com", "u9facwxv7q28", "EdSmAnU9TtbtZfv9", "ebdb");
         if ($mysqlObj->connect_errno) {
             die("Failed to connect to MySQL: " . $mysqlObj->connect_error);
         } else {
             return $mysqlObj;
         }
     }
+
+    var $obj;
+
+    public function setDbObj() {$this->obj=$this->connect();}
 
     public function queryForRow($query) {
         $row = $this->connect()->query($query)->fetch_array(MYSQLI_BOTH);
