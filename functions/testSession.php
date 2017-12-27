98036de39ff6be1108e55e36d53eecf185801dae
techybyte <info@techybyte.co.uk>
2017-12-27 18:33:53 +0000

    V1-3

--- functions/testSession.php
+++ functions/testSession.php
@@ -3,6 +3,6 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 include_once("session.php");
 echo ($_SERVER['REMOTE_ADDR']);
-echo ('INSERT INTO `sessions` (`id`, `expiry`, `userId`, `ip`) VALUES (\'' . "1" . '\', \'' . "1" . '\', \'' . "1" . '\', ' . $_SERVER["REMOTE_ADDR"] . ');');
+//echo ('INSERT INTO `sessions` (`id`, `expiry`, `userId`, `ip`) VALUES (\'' . "1" . '\', \'' . "1" . '\', \'' . "1" . '\', ' . $_SERVER["REMOTE_ADDR"] . ');');
 
 
