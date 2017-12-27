8e88c8c53be262adad089402732ca812ed6348fc
techybyte <info@techybyte.co.uk>
2017-12-27 18:39:24 +0000

    V1-3

--- home/assemblies/add/process.php
+++ home/assemblies/add/process.php
@@ -1,15 +1,21 @@
 <?php
 
 include_once("../../../functions/notice.php");
+include_once("../../../functions/session.php");
 function redirect($url, $permanent = false) { header('Location: ' . ((((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . "/" . $url), true, $permanent ? 301 : 302); exit(); }
 $db = new db();
-if (isset($_POST["title"]) && isset($_POST["body"]) && isset($_POST["dates"])) {
-    $notice = new notice();
-    foreach($_POST['dates'] as $date=>$entry) {
-        $notice->newNotice($_POST["title"], $_POST["body"], $entry);
+$session = new session();
+$session -> checkSession();
+if ($session->group->admin == 1) {
+    if (isset($_POST["date"])) {
+        if (strlen($_POST["date"]) == 8 && is_numeric($_POST["date"]))
+        $db->queryForNothing("INSERT INTO `assemblies` (date) VALUES (". $_POST["date"] . ");");
+        redirect("home/assemblies/add/?n=suc"); //SUCCESS notice
+    } else {
+        redirect("home/assemblies/add/?n=ii"); //INVALID INPUT notice
+        exit();
     }
-    redirect("home/notices/new/?n=suc"); //SUCCESS notice
 } else {
-    redirect("home/notices/new/?n=ii"); //INVALID INOUT notice
+    redirect("home/assemblies/add/?n=una"); // UNAUTH notice
     exit();
 }
